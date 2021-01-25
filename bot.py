03791b7abd372dd1beccbd1b71f35192f059a01d
VahuBali <67645126+VahuBali@users.noreply.github.com>
2021-01-25 10:19:11 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -132,7 +132,7 @@ async def ch_pr():
         await asyncio.sleep(10)
 
 client.loop.create_task(ch_pr())
-client.run()
+client.run(ch_pr())
 
 @client.group(invoke_without_command=True)
 async def help(ctx):
